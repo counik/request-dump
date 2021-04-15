bf47a232d98cdbd1b02bad68be6ae3fde9e9f66b
Kevin Counihan <kevin.counihan17@gmail.com>
2021-04-15 11:05:39 -0700

    Returns 200 OK instead of object that was saved to DB. Added routes with built in delay

--- server/controllers/requestsController.js
+++ server/controllers/requestsController.js
@@ -1,14 +1,10 @@
 const Request = require("../models/request");
 
-const saveRequest = (req, res, next) => {
-  // Todo: Transform headers into array of objects
-  // with {headerTitle: <Title>, headerValue: <Value>}
-  // Currently, headers property is transformed by front-end before
-  // being displayed
+const createReqObject = (req) => {
   const headers = req.rawHeaders;
   const queryString = req.query;
 
-  const newReq = {
+  return {
     headers,
     queryString,
     method: req.method,
@@ -19,16 +15,33 @@ const saveRequest = (req, res, next) => {
     ip: req.ip,
     ips: req.ips,
   };
+}
+
+const delay = (seconds) => {
+  return (req, res, next) => {
+    setTimeout(() => {
+      next();
+    }, seconds * 1000)
+  }
+}
+
+const saveRequest = (req, res, next) => {
+  // Todo: Transform headers into array of objects
+  // with {headerTitle: <Title>, headerValue: <Value>}
+  // Currently, headers property is transformed by front-end before
+  // being displayed
+  // Use req.params.endpoint to retrieve URL path
+  const newReq = createReqObject(req);
 
   Request.create({ data: newReq }).then((newReqFromDb) => {
-    console.log(newReqFromDb);
-    res.json(newReqFromDb);
-  });
+    res.status(200).send("OK");
+  }).catch(err => console.error(err));
 };
 
 // Add sort functionality by chaining method here
 const getRequests = (req, res, next) => {
   Request.find({})
+    .limit(15)
     .sort("-createdAt")
     .then((requests) => {
       res.json(requests);
@@ -37,3 +50,4 @@ const getRequests = (req, res, next) => {
 
 exports.saveRequest = saveRequest;
 exports.getRequests = getRequests;
+exports.delay = delay;
--- server/routes/api.js
+++ server/routes/api.js
@@ -7,5 +7,7 @@ const requestsController = require("../controllers/requestsController");
 // router.post("/dumps", dumpsController.createDump);
 // router.get("/dumps/:id", dumpsController.getDump);
 router.all("/myEndpoint", requestsController.saveRequest);
+router.all("/delay5seconds", requestsController.delay(5), requestsController.saveRequest);
+router.all("/delay100seconds", requestsController.delay(100), requestsController.saveRequest);
 router.get("/myEndpoint/inspect", requestsController.getRequests);
 module.exports = router;
